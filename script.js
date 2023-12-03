bba6f3f3f91511eb442dc34d02c39957cea86409
olhaareshetnyk <144936840+olhaareshetnyk@users.noreply.github.com>
2023-12-03 22:10:00 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+    document.body.style.background = "#e8f3fa";
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    changeHeaderColors();
+})
+
+function changeButtonColor(button) {
+
+    button.style.backgroundColor = 'blue'; 
+}
